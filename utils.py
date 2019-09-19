3d0c9f968dcb5b9ccc63583d70747c26232c8c66
zackdilan <chacko.zackdilan@gmail.com>
2019-09-19 14:39:11 +0200

    added visdom class and train_val function to utils.py file

--- utils.py
+++ utils.py
@@ -37,3 +37,79 @@ class VisdomLinePlotter(object):
             ))
         else:
             self.viz.line(X=np.array([x]), Y=np.array([y]), env=self.env, win=self.plots[var_name], name=split_name, update = 'append')
+
+
+def train_val(dataloaders,model,criterion,optimizer,num_epochs):
+    """
+    training and validation function phase for each epoch
+    Args:
+    log_dir(str): mention the path to save the logfiles
+    """
+    num_images = 0
+    best_acc = 0
+
+    for epoch in range(num_epochs):
+        print("Epoch {}/{}". format(epoch,num_epochs-1))
+        print("-"*50)
+        since = time.time()
+        running_loss = 0.0
+        total_pixels = 0
+        correct_pixels = 0
+        # Each epoch has a training and validation phase
+        for phase in ["train","val"]:
+            if phase == "train":
+                #scheduler.step()
+                model.train()  ## set model in training mode
+            else:
+                model.eval()   ## set model in validation mode
+            # define metric variables
+            running_loss = 0.0
+            total_train,correct_train  = 0
+            #iterate over the data
+            for index,sampled_batch in enumerate(dataloaders[phase]):
+                inputs = sampled_batch["image"].to(device)  # N,C,H,W
+                labels = sampled_batch["masks"]  # N,C,H.W
+                labels = torch.squeeze(labels,1).long().to(device) # N,H,W
+                num_images += inputs.size(0)
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                #forward
+                #trach history only in forward mode..
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _,predicted = torch.max(outputs,1)
+                    loss = criterion(outputs,labels)
+
+                    #backward + optimize only in training phase
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+
+                #metric evaluation
+                running_loss += loss.item()
+                total_pixels += labels.nelement()  # to get the total number of pixels in the batch
+                correct_pixels += predicted.eq(labels.data).sum().item()
+
+            # calculate the minibatch loss and accuracy and plot it using Visdom
+            epoch_loss =  running_loss / num_images
+            epoch_acc = 100 * correct_pixels / total_pixels
+            if phase == 'train':
+                plotter.plot('loss', 'train', 'Class Loss', epoch, epoch_loss)
+                plotter.plot('accuracy', 'train', 'Class Acc', epoch, epoch_acc)
+            else:
+                plotter.plot('loss', 'validation', 'Class Loss', epoch, epoch_loss)
+                plotter.plot('accuracy', 'validation', 'Class Acc', epoch, epoch_acc)
+            print("{} --- Epoch {}, Epoch  loss : {:.4f} , Accuracy : {} %".format(phase ,epoch,epoch_loss,epoch_acc))
+            # saving the model
+            if phase == "val" and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                torch.save({ "epoch": epoch,
+                            "model_state_dict":model.state_dict(),
+                            "optimizer_state_dict":optimizer.state_dict(),
+                            "loss":epoch_loss,
+                        },'/home/chacko/data/logs/Exp_3/train_exp3-epoch{}.pth'.format(epoch))
+            else:
+                pass
+
+            time_elapsed = time.time()-since
+            print("Epoch complete in {:.0f}min - {:.0f}secs".format(time_elapsed/60,time_elapsed%60))
