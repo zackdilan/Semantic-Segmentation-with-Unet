2b3706b999baff3b8b4f54ddb74fcfa79200694c
zackdilan <chacko.zackdilan@gmail.com>
2019-09-19 16:00:17 +0200

    comandline arguments

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import random
 from PIL import Image
 import copy
 import time
+import argparse
 import torch
 import torch.optim as optim
 from torch.optim import lr_scheduler
@@ -18,21 +19,52 @@ from visdom import Visdom
 
 from dataloader import get_dataloaders
 from network_1024 import Unet
-from utils import VisdomLinePlotter
+from utils import VisdomLinePlotter,train_val
 
 
+def read_flags():
+    """Return Global variables"""
+    parser = argparse.ArgumentParser()
 
-if __name__ == "__main__":
+    parser.add_argument(
+    "--dataset_dir",
+    default = '/home/chacko/data/supervisely_person_data'
+    help = "path_to/root_directory/Dataset")
+
+    parser.add_argument(
+    "--log_dir",
+    default = '/home/chacko/logs'
+    help = "path_to/log_directory/")
+
+    parser.add_argument(
+    "--train_batch_size"
+    default = 1
+    help = "define the training batch size")
+
+    parser.add_argument(
+    "--val_batch_size"
+    default = 1
+    help = "define the validation batch size")
+
+    parser.add_argument(
+    "--epochs"
+    default = 100
+    help = "define the number of epochs for training")
+
+    return parser.parse_args()
+
+def main(FLAGS):
 
     "train and validate the Unet model"
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     #data directory
-    data_dir =  '/home/chacko/data/supervisely_person_data'
+    data_dir =  FLAGS.dataset_dir
+    #log_directory
+    log_dir = FLAGS.log_dir
     # Hyper and other parameters
-    train_batch_size = 12
-    val_batch_size   = 1
-    num_epochs = 100
-    learning_rate = 0.01            #
+    train_batch_size = FLAGS.train_batch_size
+    val_batch_size   = FLAGS.val_batch_size
+    num_epochs = FLAGS.epochs
     num_classes = 2
     # get the train and validation dataloaders
     dataloaders = get_dataloaders(data_dir,train_batch_size,val_batch_size)
@@ -46,16 +78,16 @@ if __name__ == "__main__":
         print("no multiple gpu found")
     model.to(device)
     criterion = nn.CrossEntropyLoss()
-
     optimizer = optim.SGD(model.parameters(),
                           lr=0.02,
                           momentum=0.9,
                           weight_decay=0.0005)
-
-
     #optimizer = optim.Adam(model.parameters(),lr = learning_rate)
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer,step_size = 10,gamma = 0.1)
     plotter = VisdomLinePlotter(env_name='Unet Train')
     # uncomment for leraning rate schgeduler..
-    #train_val(dataloaders,model,criterion,optimizer,exp_lr_scheduler,num_epochs)
-    train_val(dataloaders,model,criterion,optimizer,num_epochs)
+    train_val(dataloaders,model,criterion,optimizer,num_epochs,log_dir)
+
+    if __name__ == '__main__':
+        flags = read_flags
+        main(flags)
